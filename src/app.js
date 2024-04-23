c4f629421b7ae1debb39c946fdea7f7a51d01126
henrique.sena <carloshenrique3250@gmail.com>
2024-04-23 20:14:25 -0300

    dev-shopping-carts
    * Incluindo novas dependencias e configurando arquivos;
    * Criando novos arquivos de controller, routes;
    * Ajuste no arquivo de banco de dados;
    * Configurando arquivo .env;

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const db = require('./config/db');
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-const PORT = 8080;
+app.use(bodyParser.json());
 
+const cartsController = require('./routes/cartsRoutes');
+app.use('/api/carts', cartsController);
 
-app.use(express.json());
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo deu errado!');
+});
 
 app.listen(PORT, () => {
-    console.log(`Exencutando projeto na porta {PORT}`);
+    console.log(`Exencutando projeto na porta ${PORT}`);
 });
