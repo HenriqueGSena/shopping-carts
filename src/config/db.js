06575f969acb199e0f890baa31dea49dff5027af
henrique.sena <carloshenrique3250@gmail.com>
2024-04-23 20:54:59 -0300

    dev-shopping-carts
    * Removendo test de conexao de banco;

--- src/config/db.js
+++ src/config/db.js
@@ -8,12 +8,4 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Erro ao conectar ao banco de dados:', err);
-        return;
-    }
-    console.log('Conexão ao banco de dados MySQL estabelecida com sucesso.');
-});
-
 module.exports = connection;
